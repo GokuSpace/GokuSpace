2762e82037a6c13dcb609c4b1ce501e76fd36c57
Jerry Renn <jerryrenn1998@gmail.com>
2023-06-10 16:18:08 -0700

    implement profile screen w/ stacks

---
+++ screens/screen-components/profile-screens/UpdateProfileScreen.tsx
@@ -0,0 +1,14 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React, { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+function UpdateProfileScreen() {
+
+  return (
+    <View >
+      <Text>Update PROFILE SCREEN</Text>
+    </View>
+  );
+}
+
+export default UpdateProfileScreen;
---
+++ screens/screen-components/profile-screens/ViewUsersPostsScreen.tsx
@@ -0,0 +1,14 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React, { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+function ViewUsersPostsScreen() {
+
+  return (
+    <View>
+      <Text>THIS IS TO SEE THE USER'S POSTS SCREEN</Text>
+    </View>
+  );
+}
+
+export default ViewUsersPostsScreen;
