fd49fb505b6c9e63825a1ec64febb2f0ae083fba
Milad Moulayi <m.moulayi123@gmail.com>
2023-06-10 16:25:15 -0700

    Tabs: All, Friends, ForYou

--- characters.js
+++ characters.js
@@ -5,6 +5,9 @@ const characters = [
     text: "Hi ya'll. 'A lesson without pain is meaningless.' - Edward Elric",
     image_url:
       "https://cdn.myanimelist.net/images/characters/5/54265.jpg?s=65c1a674adae3d7c5740d5711517285a",
+    is_friend: true,
+    series: "Fullmetal Alchemist",
+    character: "Edward Elric",
   },
   {
     mal_id: 2,
@@ -12,6 +15,9 @@ const characters = [
     text: "Don't forget, we're not gods. 'Humankind cannot gain anything without first giving something in return.' - Alphonse Elric",
     image_url:
       "https://cdn.myanimelist.net/images/characters/9/72533.jpg?s=d38cf4e2e5cbb46ddaf2b23345a03eae",
+    is_friend: true,
+    series: "Fullmetal Alchemist",
+    character: "Alphonse Elric",
   },
   {
     mal_id: 3,
@@ -19,6 +25,9 @@ const characters = [
     text: "Strength is beauty, and my muscles are a miracle of grace! - Olivier Mira Armstrong",
     image_url:
       "https://cdn.myanimelist.net/images/characters/7/83953.jpg?s=616c3cbf0f0a02068ac4f9bf7e52c746",
+    is_friend: false,
+    series: "Attack on Titan",
+    character: "Mikasa Ackerman",
   },
   {
     mal_id: 4,
@@ -26,6 +35,9 @@ const characters = [
     text: "I am a solider, but more than that, I am a woman! - Kathleen Elle Armstrong",
     image_url:
       "https://cdn.myanimelist.net/images/characters/14/80951.jpg?s=3aa6cce3cb9f8c3080771bc4960eabbf",
+    is_friend: false,
+    series: "Naruto",
+    character: "Sakura Haruno",
   },
   {
     mal_id: 5,
@@ -33,6 +45,9 @@ const characters = [
     text: "Behold! These are my ultimate abs! - Alex Louis Armstrong",
     image_url:
       "https://cdn.myanimelist.net/images/characters/2/71066.jpg?s=c20e0eaade03719e6637e381a63c8154",
+    is_friend: true,
+    series: "Fullmetal Alchemist",
+    character: "Roy Mustang",
   },
   {
     mal_id: 6,
@@ -40,6 +55,9 @@ const characters = [
     text: "I'm going to devour you! Prepare yourself, humans! - Philip Gargantos Armstrong",
     image_url:
       "https://cdn.myanimelist.net/images/characters/11/177627.jpg?s=5e7c3f6ed6d1a87c3d327a9fde6bdb68",
+    is_friend: false,
+    series: "One Piece",
+    character: "Monkey D. Luffy",
   },
   {
     mal_id: 7,
@@ -47,6 +65,9 @@ const characters = [
     text: "Killing people is fun! - Barry the Chopper",
     image_url:
       "https://cdn.myanimelist.net/images/characters/3/52723.jpg?s=ae88023ff5b97c95f191e228f6543e3c",
+    is_friend: false,
+    series: "Death Note",
+    character: "Light Yagami",
   },
   {
     mal_id: 8,
@@ -54,6 +75,9 @@ const characters = [
     text: "I just want to live freely. - Bido",
     image_url:
       "https://cdn.myanimelist.net/images/characters/3/56215.jpg?s=b5a4372968023b6dec7cd7ae283563cc",
+    is_friend: true,
+    series: "Bakugou",
+    character: "My Hero Academia",
   },
   {
     mal_id: 9,
@@ -61,11 +85,17 @@ const characters = [
     text: "Woof! - Black Hayate",
     image_url:
       "https://cdn.myanimelist.net/images/characters/12/81547.jpg?s=fc3b02fa7a612d9197588646c078cbd7",
+    is_friend: true,
+    series: "Fullmetal Alchemist",
+    character: "Riza Hawkeye",
   },
   {
     mal_id: 10,
     name: "Bobby",
     text: "I'm Bobby, the most awesome side character ever!",
+    is_friend: true,
+    series: "Bakugou",
+    character: "My Hero Academia",
     image_url:
       "https://cdn.myanimelist.net/images/characters/3/473873.jpg?s=b1eb0b078de7156c08601714498d009b",
   },
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,37 +1,79 @@
 import { useNavigation } from "@react-navigation/native";
-import { Avatar, ListItem } from "@rneui/themed";
+import { Avatar, ListItem, Tab } from "@rneui/themed";
+import { useEffect, useState } from "react";
 import React, { ScrollView, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import characters from "../characters";
 
 function HomeScreen() {
+  const [filteredCharacters, setFilteredCharacters] = useState(characters);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index === 0) {
+      setFilteredCharacters(characters);
+    }
+    if (index === 1) {
+      setFilteredCharacters(friends);
+    }
+    if (index === 2) {
+      setFilteredCharacters(forYou);
+    }
+  }, [index]);
+
+  const friends = characters.filter((char) => {
+    return char.is_friend;
+  });
+  const forYou = characters.filter((char) => {
+    return (
+      char.series === "Fullmetal Alchemist" || char.character === "Riza Hawkeye"
+    );
+  });
+
   return (
-    <ScrollView>
-      {characters.map((character) => {
-        const contentKey = Math.floor(1000000 * Math.random());
-        const nameKey = Math.floor(1000000 * Math.random());
-        const characterKey = Math.floor(1000000 * Math.random());
-        return (
-          <>
-            <ListItem key={character.mal_id} bottomDivider>
-              <Avatar
-                key={character.image_url}
-                rounded
-                source={{
-                  uri: character.image_url,
-                }}
-              />
-              <ListItem.Content key={contentKey}>
-                <ListItem.Title key={nameKey}>{character.name}</ListItem.Title>
-                <ListItem.Subtitle key={characterKey}>
-                  {character.text}
-                </ListItem.Subtitle>
-              </ListItem.Content>
-            </ListItem>
-          </>
-        );
-      })}
-    </ScrollView>
+    <>
+      <Tab
+        value={index}
+        onChange={(e) => setIndex(e)}
+        indicatorStyle={{
+          backgroundColor: "white",
+          height: 3,
+        }}
+        variant="primary"
+      >
+        <Tab.Item title="All" />
+        <Tab.Item title="Friends" />
+        <Tab.Item title="For You" />
+      </Tab>
+      <ScrollView>
+        {filteredCharacters.map((character) => {
+          const contentKey = Math.floor(1000000 * Math.random());
+          const nameKey = Math.floor(1000000 * Math.random());
+          const characterKey = Math.floor(1000000 * Math.random());
+          return (
+            <>
+              <ListItem key={character.mal_id} bottomDivider>
+                <Avatar
+                  key={character.image_url}
+                  rounded
+                  source={{
+                    uri: character.image_url,
+                  }}
+                />
+                <ListItem.Content key={contentKey}>
+                  <ListItem.Title key={nameKey}>
+                    {character.name}
+                  </ListItem.Title>
+                  <ListItem.Subtitle key={characterKey}>
+                    {character.text}
+                  </ListItem.Subtitle>
+                </ListItem.Content>
+              </ListItem>
+            </>
+          );
+        })}
+      </ScrollView>
+    </>
   );
 }
 
