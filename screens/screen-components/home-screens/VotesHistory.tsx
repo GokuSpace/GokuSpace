5cd8a9e6ae0b3613f5bc8d78d56970aa154aaee5
Tammam Tautou <tautoutammam@gmail.com>
2023-06-11 04:57:04 -0700

    make VotesHistory as shared component

---
+++ screens/screen-components/home-screens/VotesHistory.tsx
@@ -0,0 +1,30 @@
+import { Avatar, ListItem } from "@rneui/themed";
+import React, { ScrollView } from "react-native-gesture-handler";
+
+
+const VotesHistory = (user) => {
+  return (
+    <ScrollView>
+      {
+        user.votes.map(vote => {
+          return (
+            <>
+              <ListItem >
+                <Avatar rounded
+                  source={{
+                    uri: vote.url
+                  }} />
+                  <ListItem.Content >
+                    <ListItem.Title>{vote.series}</ListItem.Title>
+                    <ListItem.Subtitle>{vote.created_at}</ListItem.Subtitle>
+                  </ListItem.Content>
+              </ListItem>
+            </>
+          );
+        })
+      }
+    </ScrollView>
+  );
+}
+
+export default VotesHistory;
