2df41fa4f819efdee437cf1e69c3ca03384caaca
Jerry Renn <jerryrenn1998@gmail.com>
2023-06-11 11:24:00 -0700

    implement add image functionality that allows users to edit prof pics

--- screens/screen-components/profile-screens/EditProfilePictureScreen.tsx
+++ screens/screen-components/profile-screens/EditProfilePictureScreen.tsx
@@ -1,13 +1,59 @@
-import { useNavigation, useRoute } from "@react-navigation/native";
-import React, { Text, View } from "react-native";
+import React, { Text, View, Image} from "react-native";
+import { useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
 import { TouchableOpacity } from "react-native-gesture-handler";
+import tw from 'tailwind-react-native-classnames';
 
-function EditProfilePictureScreen() {
+const EditProfilePictureScreen: React.FC = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  const handleImageSelect = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
 
   return (
-    <View>
-      <Text>THIS IS THE PROFILE PICTURE SCREEN</Text>
+    <View style={tw`flex-1 items-center justify-center`}>
+      <Text style={tw`text-2xl font-bold mb-8`}>THIS IS THE PROFILE PICTURE SCREEN</Text>
+      <View style={tw`items-center`}>
+        <TouchableOpacity
+          style={tw`w-48 h-48 rounded-lg bg-gray-300 mb-4 items-center justify-center`}
+          onPress={handleImageSelect}
+        >
+          {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        </TouchableOpacity>
+      </View>
+
+      <View style={tw`flex-row`}>
+        <View style={tw`items-center`}>
+          <TouchableOpacity
+            style={tw`w-48 h-48 rounded-lg bg-gray-300 mb-4 items-center justify-center`}
+            onPress={handleImageSelect}
+          >
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+          </TouchableOpacity>
+        </View>
+
+        <View style={tw`items-center`}>
+          <TouchableOpacity
+            style={tw`w-48 h-48 rounded-lg bg-gray-300 mb-4 items-center justify-center`}
+            onPress={handleImageSelect}
+          >
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
     </View>
   );
 }
