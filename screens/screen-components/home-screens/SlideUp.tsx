b0494d71ab322f166f19298ad357181e1d330307
Tammam Tautou <tautoutammam@gmail.com>
2023-06-11 03:26:35 -0700

    refactor slidUp to slide only half screen height

--- screens/screen-components/home-screens/SlideUp.tsx
+++ screens/screen-components/home-screens/SlideUp.tsx
@@ -1,28 +1,34 @@
-import React, { useRef }  from 'react';
-import {View, Button, Text } from 'react-native';
+import React, { useRef, useState }  from 'react';
+import {View, Button, Text, Dimensions, ViewStyle } from 'react-native';
+import { useSafeAreaFrame } from 'react-native-safe-area-context';
 import SlidingUpPanel from 'rn-sliding-up-panel';
-import SLidingUpPanel from 'rn-sliding-up-panel';
 
-const styles = {
+const styles: {container: ViewStyle} = {
   container: {
     flex: 1,
     backgroundColor: 'white',
-    aligneIems: 'center',
+    alignItems: 'flex-end',
     justifyContent: 'center'
   }
 };
 
 const  SlideUp = () => {
 const _panel = useRef(null);
+const { height } = Dimensions.get('window');
+const [draggableRange, setDraggableRange] = useState({
+  top: (0.55 * height),
+  bottom: 0
+})
+
+
 return (
   <View style={styles.container}>
-    <Button title="show panel" onPress={() => _panel.current.show()}/>
-    <SlidingUpPanel ref={_panel}>
+    <Button title="show panel" onPress={() =>_panel.current.show()}/>
+    <SlidingUpPanel ref={_panel} draggableRange={draggableRange}>
       <View style={styles.container}>
         <Text> it works</Text>
-        <Button title="hide panle" onPress={() => _panel.current.hide()}/>
+        <Button title="hide panle" onPress={() =>_panel.current.hide()}/>
       </View>
-
     </SlidingUpPanel>
   </View>
 );
