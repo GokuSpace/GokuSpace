a7146c3962b1bf3eee2e1d07c734005978139d75
Jerry Renn <jerryrenn1998@gmail.com>
2023-06-12 17:40:50 -0700

    add screen nagivation to event details page

--- screens/screen-components/events-screens/EventDetails.tsx
+++ screens/screen-components/events-screens/EventDetails.tsx
@@ -11,8 +11,18 @@ const EventDetails = () => {
   return (
     <>
       <Text>{event.name}</Text>
+      <Text>{event.startDate}</Text>
     </>
   );
 }
 
 export default EventDetails;
+
+/*
+ WARN  Non-serializable values were found in the navigation state. Check:
+
+Events > EventDetails > params.event.startDate (Sun Jun 18 2023 08:00:00 GMT-0700)
+
+This can break usage such as persisting and restoring state. This might happen if you passed non-serializable values such as function, class instances etc. in params. If you need to use components with callbacks in your options, you can use 'navigation.setOptions' instead. See https://reactnavigation.org/docs/troubleshooting#i-get-the-warning-non-serializable-values-were-found-in-the-navigation-state for more details.
+
+*/
