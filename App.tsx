5b0236084be4117eea042b13e0cd8db340bdba28
Jerry Renn <jerryrenn1998@gmail.com>
2023-06-13 16:18:16 -0700

    fixed merge conflicts again

--- App.tsx
+++ App.tsx
@@ -3,10 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { ThemeProvider, createTheme } from '@rneui/themed';
 import { StatusBar } from "expo-status-bar";
-<<<<<<< HEAD
-import { useState } from 'react';
-=======
->>>>>>> a0ac32ea51391cf65f32e1ecfce63cbee7413e62
 import React, { Text, View } from "react-native";
 import { useState, createContext } from 'react';
 import "react-native-gesture-handler";
@@ -20,12 +16,8 @@ import SignupScreen from './screens/auth/SignupScreen';
 import EditProfilePictureScreen from "./screens/screen-components/profile-screens/EditProfilePictureScreen";
 import UpdateProfileScreen from "./screens/screen-components/profile-screens/UpdateProfileScreen";
 import ViewUsersPostsScreen from "./screens/screen-components/profile-screens/UsersPosts";
-<<<<<<< HEAD
-import VoteScreen from "./screens/screen-components/profile-screens/VoteScreen";
-import EventDetails from "./screens/screen-components/events-screens/EventDetails";
-=======
 import VotesHistory from "./screens/screen-components/home-screens/VotesHistory";
->>>>>>> a0ac32ea51391cf65f32e1ecfce63cbee7413e62
+import EventDetails from "./screens/screen-components/events-screens/EventDetails";
 
 // Okay okay
 
@@ -49,79 +41,40 @@ export default function AppTabs() {
   if (loggedIn) {
 
     return (
-<<<<<<< HEAD
-      <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Friends" component={FriendScreen} />
-          <Tab.Screen name="Profile" options={{ headerShown: false }}>
-            {() => (
-              <Stack.Navigator>
-                <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-                <Stack.Screen name="EditProfilePictureScreen" component={EditProfilePictureScreen} />
-                <Stack.Screen name="UpdateProfileScreen" component={UpdateProfileScreen} />
-                <Stack.Screen name="ViewUsersPostsScreen" component={ViewUsersPostsScreen} />
-                <Stack.Screen name="VoteScreen" component={VoteScreen} />
-              </Stack.Navigator>
-            )}
-          </Tab.Screen>
-          <Tab.Screen name="Trending" component={TrendingScreen} />
-          <Tab.Screen name="Events" options={{ headerShown: false }}>
-            {() => (
-              <Stack.Navigator>
-                <Stack.Screen name="EventsScreen" component={EventsScreen} />
-                <Stack.Screen name="EventDetails" component={EventDetails} />
-              </Stack.Navigator>
-            )}
-          </Tab.Screen>
-        </Tab.Navigator>
-      </NavigationContainer >
-      </ThemeProvider>
-    )
-  } else {
-    return (
-      <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login">
-            {(props) => <LoginScreen {...props} setLoggedIn={setLoggedIn} />}
-          </Stack.Screen>
-          <Stack.Screen name="SignUp">
-            {(props) => <SignupScreen {...props} setLoggedIn={setLoggedIn} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-      </ThemeProvider>
-
-=======
-      <userContext.Provider value={ { currentUser, setCurrentUser } }>
+      <userContext.Provider value={{ currentUser, setCurrentUser }}>
         <ThemeProvider theme={theme}>
           <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Friends" component={FriendScreen} />
-            <Tab.Screen name="Profile" options={{ headerShown: false }}>
+            <Tab.Navigator>
+              <Tab.Screen name="Home" component={HomeScreen} />
+              <Tab.Screen name="Friends" component={FriendScreen} />
+              <Tab.Screen name="Profile" options={{ headerShown: false }}>
                 {() => (
                   <Stack.Navigator>
                     <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
                     <Stack.Screen name="EditProfilePictureScreen" component={EditProfilePictureScreen} />
                     <Stack.Screen name="UpdateProfileScreen" component={UpdateProfileScreen} />
                     <Stack.Screen name="ViewUsersPostsScreen" component={ViewUsersPostsScreen} />
-                    <Stack.Screen name="VoteScreen" component={VoteScreen} />
+                    <Stack.Screen name="VotesHistory" component={VotesHistory} />
                   </Stack.Navigator>
                 )}
-            </Tab.Screen>
-            <Tab.Screen name="Trending" component={TrendingScreen} />
-            <Tab.Screen name="Events" component={EventsScreen} />
-          </Tab.Navigator>
-          </NavigationContainer>
+              </Tab.Screen>
+              <Tab.Screen name="Trending" component={TrendingScreen} />
+              <Tab.Screen name="Events" options={{ headerShown: false }}>
+                {() => (
+                  <Stack.Navigator>
+                    <Stack.Screen name="EventsScreen" component={EventsScreen} />
+                    <Stack.Screen name="EventDetails" component={EventDetails} />
+                  </Stack.Navigator>
+                )}
+              </Tab.Screen>
+            </Tab.Navigator>
+          </NavigationContainer >
         </ThemeProvider>
       </userContext.Provider>
-      )
-    } else {
+    )
+  } else {
     return (
-      <userContext.Provider value={ { currentUser, setCurrentUser } }>
+      <userContext.Provider value={{ currentUser, setCurrentUser }}>
         <ThemeProvider theme={theme}>
           <NavigationContainer>
             <Stack.Navigator>
@@ -135,7 +88,6 @@ export default function AppTabs() {
           </NavigationContainer>
         </ThemeProvider>
       </userContext.Provider>
->>>>>>> a0ac32ea51391cf65f32e1ecfce63cbee7413e62
     );
 
   }
