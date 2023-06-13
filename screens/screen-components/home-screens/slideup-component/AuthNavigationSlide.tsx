651aebb36984776edde51367129636dfbbe0a844
Tammam Tautou <tautoutammam@gmail.com>
2023-06-13 12:11:24 -0700

    refactor

---
+++ screens/screen-components/home-screens/slideup-component/AuthNavigationSlide.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Image, View } from 'react-native';
+import GestureRecognizer, {
+} from 'react-native-swipe-gestures';
+import userProfilePics from '../../../../userProfilePics';
+
+
+
+
+const AuthNavigationSlide = () => {
+  const [index, setIndex] = useState(0);
+
+  const onSwipeLeft = () => {
+    index === 0 ? setIndex(1) : setIndex(0);
+  };
+  const onSwipeRight = () => {
+    index === 0 ? setIndex(1) : setIndex(0);
+  };
+
+  const config = {
+    velocityThreshold: 0.3,
+    directionalOffsetThreshold: 80,
+  };
+  return (
+    <GestureRecognizer
+      onSwipeLeft={onSwipeLeft}
+      onSwipeRight={onSwipeRight}
+      config={config}
+      style={{ flex: 1 }}
+    >
+      <View style={{ flex: 1 }}>
+        <Image
+          source={{
+            uri: userProfilePics[index],
+            width: 100,
+            height: 100,
+          }}
+        />
+
+      </View>
+    </GestureRecognizer>
+  );
+}
+
+export default AuthNavigationSlide;
+
