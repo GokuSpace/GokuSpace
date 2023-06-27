02e365dacbc8654c3819ed1733ca1b3102fc0fc1
Tammam Tautou <tautoutammam@gmail.com>
2023-06-27 00:31:57 -0700

    ok

--- screens/screen-components/trending-screen/Week.tsx
+++ screens/screen-components/trending-screen/Week.tsx
@@ -1,24 +1,34 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 import React from "react";
+import { View, Text, Pressable } from "react-native";
+import { ListItem, Avatar } from "react-native-elements";
 import most_3_trending_by_date from "./most_3_trending_by_date";
+import animeSeries from "../../../animeSeries";
 import { ScrollView } from "react-native-gesture-handler";
-import WeekEntry from "./WeekEntry";
-import { v4 as uuidv4 } from 'uuid';
 
-const Week = ({ animeSeries, setSingleDay, setDayMostTrending }) => {
+const Week = ({ animeSeries }) => {
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const currentDate = new Date();
   const currentWeekDay = currentDate.getDay();
   const currentMonthDay = currentDate.getDate()
+
 
   return (
     <ScrollView >
       {daysOfWeek.map((day, index) => {
         const { first, second, third } = most_3_trending_by_date(animeSeries, currentDate.setDate(currentMonthDay - currentWeekDay + index));
 
-        // Include the key prop with unique value using uuidv4
-        return (<WeekEntry key={uuidv4()} day={day} first={first} second={second} third={third} setSingleDay={setSingleDay} setDayMostTrending={setDayMostTrending} />);
+        return (
+
+          <Pressable onPress={e => console.log('>>>>>>>>>>>>>>>>>',e)}>
+            <Text>{day}</Text>
+            <Avatar rounded source={{ uri: first.url }} size={100} />
+            <Avatar rounded source={{ uri: second.url }} size={100} />
+            <Avatar rounded source={{ uri: third.url }} size={100} />
+          </Pressable>
+
+        );
       })}
     </ScrollView>
   );
